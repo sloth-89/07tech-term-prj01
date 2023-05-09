ec223e952be53d926c3b53b5a5bcef0cfca4c8bd
restvoice2020@gmail.com <restvoice2020@gmail.com>
2023-05-09 15:48:42 +0900

    230509

---
+++ src/component/Header.js
@@ -0,0 +1,16 @@
+
+export default function Header (){
+    return(
+        <div>
+            <header>
+                <h1>
+                    <a href="/">Tech 기술 용어</a>
+                </h1>
+                <div>
+                    <a href="/" className="link">단어 추가</a>
+                    <a href="/" className="link">day 추가</a>
+                </div>
+            </header>
+        </div>
+    )
+}
