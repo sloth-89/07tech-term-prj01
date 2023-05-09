1317eeab6bf6ff1f63d9be02fad9e4a039537806
restvoice2020@gmail.com <restvoice2020@gmail.com>
2023-05-09 17:28:56 +0900

    230509

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './component/Header';
+import DayList from './component/DayList';
+import Day from './component/Day';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <DayList/>
+      <Day/>
     </div>
   );
 }
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,16 +1,14 @@
 
 export default function Header (){
     return(
-        <div>
-            <header>
-                <h1>
-                    <a href="/">Tech 기술 용어</a>
-                </h1>
-                <div>
-                    <a href="/" className="link">단어 추가</a>
-                    <a href="/" className="link">day 추가</a>
-                </div>
-            </header>
+        <div className="header">
+            <h1>
+                <a href="/">Tech 기술 용어</a>
+            </h1>
+            <div className="menu">
+                <a href="/" className="link">단어 추가</a>
+                <a href="/" className="link">day 추가</a>
+            </div>
         </div>
     )
 }
